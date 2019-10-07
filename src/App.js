a87220d4e7e62c69a75353f611e744d918090f9a
Joe <josephc.lambert@gmail.com>
2019-10-07 13:12:28 -0700

    creates ProductsContext. wraps Routes in productContext Provider

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import ProductContext from './contexts/ProductContext';
+
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
@@ -21,9 +24,10 @@ function App() {
       <Navigation cart={cart} />
 
       {/* Routes */}
-      <Route exact path='/' render={() => <Products products={products} addItem={addItem} />} />
-
-      <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Route exact path='/' render={() => <Products products={products} addItem={addItem} />} />
+        <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+      </ProductContext.Provider>
     </div>
   );
 }
