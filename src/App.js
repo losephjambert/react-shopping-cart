ffb2ac2c35b67b75550db5ba03c34707e893f600
Joe <josephc.lambert@gmail.com>
2019-10-07 13:19:45 -0700

    adds Cart Context to Navigation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Contexts
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
   const [products] = useState(data);
@@ -21,12 +22,13 @@ function App() {
 
   return (
     <div className='App'>
-      <Navigation cart={cart} />
-
-      {/* Routes */}
       <ProductContext.Provider value={{ products, addItem }}>
-        <Route exact path='/' component={Products} />
-        <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={{ cart }}>
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path='/' component={Products} />
+          <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
