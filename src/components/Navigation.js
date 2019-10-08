08c90a5996a6afaf4c579cf589e6e6d626c60344
Joe <josephc.lambert@gmail.com>
2019-10-08 11:08:32 -0700

    fixes cart total bug in navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,11 +6,14 @@ import CartContext from '../contexts/CartContext';
 
 const Navigation = () => {
   const { cart } = useContext(CartContext);
+  const getTotalCartCount = () => {
+    return cart.reduce((a, b) => a + b.quantity, 0);
+  };
   return (
     <div className='navigation'>
       <NavLink to='/'>Products</NavLink>
       <NavLink to='/cart'>
-        Cart <span>{cart.length}</span>
+        Cart <span>{getTotalCartCount()}</span>
       </NavLink>
     </div>
   );
